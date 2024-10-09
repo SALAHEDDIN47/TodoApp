8b9dd4f02c61a765ce2258c7e17fbebb7fb6e4fa
SALAHEDDIN47 <salaheddinejalloul538@gmail.com>
2024-10-09 13:11:29 +0100

    working on script v1.0.3

--- script.js
+++ script.js
@@ -34,5 +34,6 @@ taskForm.addEventListener("submit", (e) => {
 
 const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
 const taskObj = {
-    id: titleInput.value.toLowerCase(),
+    id: titleInput.value.toLowerCase().split(" "),
 }
+
