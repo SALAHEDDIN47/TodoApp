9feb072568dff74fe86f4f66bc162a25012e927d
SALAHEDDINE <salaheddinejalloul888@gmail.com>
2024-10-09 11:13:16 +0100

    working on script v1.0.1

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ let currentTask = {};
 openTaskFormBtn.addEventListener("click", () =>
   taskForm.classList.toggle("hidden")
 );
+
+closeTaskFormBtn.addEventListener("click", () => { 
+    confirmCloseDialog.showModal();
+});
+
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
+
+discardBtn.addEventListener("click", () => {
+    confirmCloseDialog.close();
+    taskForm.classList.toggle("hidden");
+});
+
+taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
