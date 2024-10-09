73b142b4620fcdb6cf6f163abf0aeb563a212c78
SALAHEDDINE <salaheddinejalloul888@gmail.com>
2024-10-09 11:26:25 +0100

    working on script v1.0.2

--- script.js
+++ script.js
@@ -31,3 +31,8 @@ discardBtn.addEventListener("click", () => {
 taskForm.addEventListener("submit", (e) => {
     e.preventDefault();
 })
+
+const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+const taskObj = {
+    id: titleInput.value.toLowerCase(),
+}
