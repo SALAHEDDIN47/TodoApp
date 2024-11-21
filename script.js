01efc19463167de3d0fcf44fa33dd89ce4fb905a
SALAHEDDIN47 <salaheddinejalloul538@gmail.com>
2024-11-21 15:01:52 +0100

    Modifying the script

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//variables declaration
 const taskForm = document.getElementById("task-form");
 const confirmCloseDialog = document.getElementById("confirm-close-dialog");
 const openTaskFormBtn = document.getElementById("open-task-form-btn");
@@ -14,13 +13,21 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = JSON.parse(localStorage.getItem("data")) || [];
 let currentTask = {};
 
+const removeSpecialChars = (val) => {
+  return val.trim().replace(/[^A-Za-z0-9\-\s]/g, '')
+}
+
 const addOrUpdateTask = () => {
+   if(!titleInput.value.trim()){
+    alert("Please provide a title");
+    return;
+  }
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
   const taskObj = {
-    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-    title: titleInput.value,
+    id:`${removeSpecialChars(titleInput.value).toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: removeSpecialChars(titleInput.value),
     date: dateInput.value,
-    description: descriptionInput.value,
+    description: removeSpecialChars(descriptionInput.value)
   };
 
   if (dataArrIndex === -1) {
@@ -39,7 +46,7 @@ const updateTaskContainer = () => {
 
   taskData.forEach(
     ({ id, title, date, description }) => {
-        tasksContainer.innerHTML += `
+        (tasksContainer.innerHTML += `
         <div class="task" id="${id}">
           <p><strong>Title:</strong> ${title}</p>
           <p><strong>Date:</strong> ${date}</p>
@@ -47,7 +54,7 @@ const updateTaskContainer = () => {
           <button onclick="editTask(this)" type="button" class="btn">Edit</button>
           <button onclick="deleteTask(this)" type="button" class="btn">Delete</button> 
         </div>
-      `
+      `)
     }
   );
 };
@@ -80,7 +87,7 @@ const editTask = (buttonEl) => {
 }
 
 const reset = () => {
-addOrUpdateTaskBtn.innerText='Add Task';
+  addOrUpdateTaskBtn.innerText = "Add Task";
   titleInput.value = "";
   dateInput.value = "";
   descriptionInput.value = "";
